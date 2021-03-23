5d730c1b4f2003492666ec70a554a38a7ccdc1b6
zhiwen <lzw5399@gmail.com>
2021-03-23 15:25:07 +0800

    fix: fix bug

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def listdir(path, list_name):
         file_path = os.path.join(path, file)
         if os.path.isdir(file_path):
             listdir(file_path, list_name)
-        elif os.path.splitext(file_path)[1] == '.pdf':
+        elif os.path.splitext(file_path)[1].lower() == '.pdf':
             list_name.append(file_path)
 
 
